7601664f6d854bcd090596a2b4042a0043d3a021
nelsonic <nelson+github@dwyl.com>
2023-07-30 19:08:45 +0100

    add earmark and html_sanitize_ex for parsing Markdown and stripping HTML #64

--- mix.exs
+++ mix.exs
@@ -9,7 +9,17 @@ defmodule Amemo.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        c: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.json": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        t: :test
+      ]
     ]
   end
 
@@ -45,7 +55,18 @@ defmodule Amemo.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+
+      # Markdown parsing/rendering: github.com/pragdave/earmark
+      {:earmark, "~> 1.4.38"},
+
+      # Remove unwanted HTML: github.com/rrrene/html_sanitize_ex
+      {:html_sanitize_ex, "~> 1.4.2"},
+
+      # Track test coverage: github.com/parroty/excoveralls
+      {:excoveralls, "~> 0.16.0", only: [:test, :dev]},
+
+
     ]
   end
 
@@ -63,7 +84,10 @@ defmodule Amemo.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      c: ["coveralls.html"],
+      s: ["phx.server"],
+      t: ["test"]
     ]
   end
 end
