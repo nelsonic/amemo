927e04b184ba075f16d7728b8ad19cb5d8d3d7cd
nelsonic <nelson+github@dwyl.com>
2023-07-31 09:14:42 +0100

    Full Markdown support in LiveView Editor #64 for https://github.com/dwyl/mvp/issues/141

--- lib/amemo_web/live/editor.ex
+++ lib/amemo_web/live/editor.ex
@@ -2,49 +2,53 @@ defmodule AmemoWeb.Editor do
   use AmemoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :val, 0)}
-  end
-
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
-  end
-
-  def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
-  end
-
-  def render(assigns) do
     md = """
-    ## Hello World!
+    # Hello World!
 
-    This is my text
+    Type some text ...
     [link](https://mvp.fly.dev/)
     https://mvp.fly.dev/
     """
+    socket = assign(socket, :md, md)
+    {:ok, assign(socket, :val, 0)}
+  end
 
+  def handle_event("render", %{"text" => text}, socket) do
+    dbg(text)
+    {:noreply, assign(socket, :md, text)}
+  end
 
+  def render(assigns) do
     ~H"""
-    <div class="markdown">
-    <%= Phoenix.HTML.raw(to_html(md)) %>
+    <form action="#" phx-change="render" phx-submit="render">
+      <textarea rows="5"
+        class="w-full my-2 py-1 px-1 text-slate-800 text-2xl
+          bg-white bg-clip-padding
+          resize-none hover:resize-y focus:resize-y
+          transition ease-in-out
+          border border-b border-slate-200
+          focus:border-none focus:outline-none
+          "
+        name="text"
+        id="text"
+        phx-debounce="100"
+        placeholder="Capture what is on your mind ..."
+      >
+        <%= @md %>
+      </textarea>
+    </form>
 
-    <h1 class="text-4xl font-bold text-center"> The count is: <%= @val %> </h1>
-     <.button phx-click="dec">-</.button>
-     <.button phx-click="inc">+</.button>
-     </div>
+    <br /> <hr /> <br />
+
+    <div class="markdown">
+      <%= Phoenix.HTML.raw(to_html(String.trim(@md))) %>
+    </div>
     """
   end
 
   def to_html(markdown) do
     markdown
-    |> Earmark.as_html!(earmark_options())
+    |> Earmark.as_html!()
     |> HtmlSanitizeEx.html5()
-  end
-
-
-  defp earmark_options() do
-    %Earmark.Options{
-      code_class_prefix: "lang-",
-      smartypants: false
-    }
   end
 end
