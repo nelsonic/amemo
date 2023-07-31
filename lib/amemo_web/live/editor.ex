5eeac963a90f06cbcddcf68cefc3e3c498aa1738
nelsonic <nelson+github@dwyl.com>
2023-07-31 07:17:56 +0100

    Markdown Parsing *WORKING* in LiveView! (it was so simple!) https://github.com/nelsonic/amemo/issues/64

--- lib/amemo_web/live/editor.ex
+++ lib/amemo_web/live/editor.ex
@@ -14,13 +14,37 @@ defmodule AmemoWeb.Editor do
   end
 
   def render(assigns) do
+    md = """
+    ## Hello World!
+
+    This is my text
+    [link](https://mvp.fly.dev/)
+    https://mvp.fly.dev/
+    """
+
 
     ~H"""
     <div class="markdown">
+    <%= Phoenix.HTML.raw(to_html(md)) %>
+
     <h1 class="text-4xl font-bold text-center"> The count is: <%= @val %> </h1>
      <.button phx-click="dec">-</.button>
      <.button phx-click="inc">+</.button>
      </div>
     """
+  end
+
+  def to_html(markdown) do
+    markdown
+    |> Earmark.as_html!(earmark_options())
+    |> HtmlSanitizeEx.html5()
+  end
+
+
+  defp earmark_options() do
+    %Earmark.Options{
+      code_class_prefix: "lang-",
+      smartypants: false
+    }
   end
 end
