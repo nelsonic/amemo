9f26c74f1aaf7a07f46bc78a536157201172b272
nelsonic <nelson+github@dwyl.com>
2023-08-04 10:29:31 +0100

    bump version of link to 1.0.7 https://github.com/dwyl/link/issues/7

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Amemo.MixProject do
       {:html_sanitize_ex, "~> 1.4.2"},
 
       # Shorten links: github.com/dwyl/link
-      {:link, "~> 1.0.6"},
+      {:link, "~> 1.0.7"},
 
       # Track test coverage: github.com/parroty/excoveralls
       {:excoveralls, "~> 0.16.0", only: [:test, :dev]}
